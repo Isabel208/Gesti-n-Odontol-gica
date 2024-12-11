fd823ff1588be8add41f234a9505691659bb31ca
Isabel208 <tovarisabela55@gmail.com>
2024-12-11 16:16:05 -0500

    Update cliente.java

---
+++ UMLjava/cliente.java
@@ -0,0 +1,20 @@
+public class cliente {
+  
+  private int codigo;
+  private String razonSocial;
+
+public Cliente () {
+}
+public int getCodigo () {
+  return codigo;
+}
+public void setCodigo (int val) {
+  this.codigo = val;
+}
+public String getRazonSocial () {
+  return razonSocial;
+}
+public vooid setRazonSocial (String val) {
+  this.razonSocial = val;
+}
+}
