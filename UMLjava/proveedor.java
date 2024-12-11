065f77d508bb4d2c9a6f051db27be1b830f8d7e3
Isabel208 <tovarisabela55@gmail.com>
2024-12-11 14:55:02 -0500

    Update proveedor.java

---
+++ UMLjava/proveedor.java
@@ -0,0 +1,24 @@
+public class Proveedor {
+
+    private int codigo;
+    private String razonSocial;
+
+    public Proveedor() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int val) {
+        this.codigo = val;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String val) {
+        this.razonSocial = val;
+    }
+}
