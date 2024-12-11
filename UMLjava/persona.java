3891a34669ed7850db05e9801272f19da2a9e6f0
Isabel208 <tovarisabela55@gmail.com>
2024-12-11 17:04:27 -0500

    Update persona.java

---
+++ UMLjava/persona.java
@@ -0,0 +1,19 @@
+public class Persona {
+  private String nombre; //propiedad
+  private int edad; 
+
+  public Persona () {
+  }
+  public int getEdad () {
+    return edad;
+  }
+  public void setEdad (int val) {
+    this.edad = val;
+  }
+  public String getNombre () {
+    return nombre;
+  }
+  public void setNombre (String val) {
+    this.nombre = val;
+  }
+}
